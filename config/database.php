31b80bfc3f00e177704f3b8c46fe5cfb924a9bf6
bringforthjoy101 <adelugba.emma@gmail.com>
2020-02-19 18:16:53 +0100

    modified database.php

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'postgres://fptfblchizmjjs:09ec7a4e0d037f4a7a3910fc85401f9f20a95a31a1bbcf81a161e59a16d93fc1@ec2-54-197-34-207.compute-1.amazonaws.com:5432/d85a49d5ins3f3'),
+            'host' => env('DB_HOST', 'ec2-54-197-34-207.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd85a49d5ins3f3'),
+            'username' => env('DB_USERNAME', 'fptfblchizmjjs'),
+            'password' => env('DB_PASSWORD', '09ec7a4e0d037f4a7a3910fc85401f9f20a95a31a1bbcf81a161e59a16d93fc1'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
